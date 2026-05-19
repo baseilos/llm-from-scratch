5063bf98495d77fa135e2eb0ab2fe2f9f9fc1bc0
Jozef Lang <contact@jozeflang.com>
2026-05-20 00:22:24 +0200

    Self-attention improvements

--- attention.py
+++ attention.py
@@ -10,17 +10,14 @@ def self_attention():
          [0.05, 0.80, 0.55]]
     )
 
-    query = inputs[1]
-    attn_scores_2 = torch.empty(inputs.shape[0])
-    for i, x_i in enumerate(inputs):
-        attn_scores_2[i] = torch.dot(query, x_i)
-    attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
-    print(attn_weights_2)
+    # Attention scores are dot products of inputs and inputs transposed
+    attn_scores = inputs @ inputs.t()
+    attn_weights = torch.softmax(attn_scores, dim=-1)
+    print(attn_scores)
+    print(attn_weights)
 
-    context_vec_2 = torch.zeros(query.shape)
-    for i, x_i in enumerate(inputs):
-        context_vec_2 += attn_weights_2[i] * x_i
-    print(context_vec_2)
+    context = attn_weights @ inputs
+    print(context)
 
 if __name__ == "__main__":
     self_attention()
