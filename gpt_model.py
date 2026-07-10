fe354d2ba7bec443a89aa6f028ce3f15d7219fdf
Jozef Lang <contact@jozeflang.com>
2026-07-10 23:16:26 +0200

    Initial GPT token generation

--- gpt_model.py
+++ gpt_model.py
@@ -93,6 +93,17 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
 if __name__ == "__main__":
     tokenizer = bpe_tokenizer.BPETokenizer()
     batch = []
@@ -105,8 +116,10 @@ if __name__ == "__main__":
 
     torch.manual_seed(123)
     model = GPTModel(GPT_CONFIG_124M)
-    out = model(batch)
+    model.eval()
+    out = generate_text_simple(model, batch, 6, GPT_CONFIG_124M["context_length"])
 
-    print("Input batch: ", batch)
-    print("Output shape: ", out.shape)
-    print(out)
+    for token_ids in out.tolist():
+        print(token_ids)
+        decoded_text = tokenizer.decode(token_ids)
+        print(decoded_text)
