7787d7eda07d2cc8e2b0e28b7a165da494b881a8
Jozef Lang <contact@jozeflang.com>
2026-07-12 13:10:31 +0200

    Initial training loop

--- llm.py
+++ llm.py
@@ -29,6 +29,16 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
 
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.position_embedding.weight.shape[0]
+    encoded = text_to_token(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model, encoded, 50, context_size)
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch = input_batch.to(device)
     target_batch = target_batch.to(device)
@@ -52,6 +62,35 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         total_loss += loss.item()
     return total_loss / num_batches
 
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_token_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.shape.numel()
+            global_step += 1
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_token_seen.append(tokens_seen)
+                print(f"Epoch {epoch+1}, Step {global_step:06d}, Train Loss: {train_loss:.3f}, Val Loss: {val_loss:.3f}")
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_token_seen
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,
@@ -67,6 +106,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = BPETokenizer()
     model = GPTModel(GPT_CONFIG_124M)
+    model.to(device)
 
     file_path = "data/the-verdict.txt"
     with open(file_path, "r", encoding="utf_8") as file:
@@ -100,8 +140,6 @@ if __name__ == "__main__":
                                                  shuffle=True,
                                                  run_workers=0)
 
-    with torch.no_grad():
-        train_loss = calc_loss_loader(train_loader, model, device)
-        val_loss = calc_loss_loader(val_loader, model, device)
-    print("Train loss:", train_loss)
-    print("Val loss:", val_loss)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.1)
+    numm_epochs = 10
+    train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimizer, device, numm_epochs, 5, 5, "Every effort moves you", tokenizer)
